bb1e4b2630e954ee18fa317e79e6c9c849d5f5d1
FrankLikuohao <frank.likuohao@gmail.com>
2017-03-07 21:26:37 +0800

    輸入數字-錯誤處理

---
+++ example3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sql = 
+                @"select from table
+            where id>1000";
+            string xml =
+                @"<Pokemon>
+                    <NationNo>#001</NationalNo>
+                    <Name>Bulbsaur</Name>
+                  </Pokemon>";
+            string json =
+                @"Pokemon = {
+                    NationalNo:'#001'
+                    Name:Bulbsaur
+                    }";
+
+        }
+    }
+}
---
+++ example4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 0;
+            int number2 = 0;
+            Console.Write("請輸入第一個數字:");
+            try
+            {
+                number1 = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.Write("請輸入第二個數字:");
+            try
+            {
+                number2 = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("{0} + {1} = {2}", number1, number2, number1 + number2);
+        }
+    }
+}
